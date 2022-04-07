9fbd64547112a031962fd087c4aa8427c92f0cd7
Pablo Uribe <sistemaspablo21@gmail.com>
2022-04-07 02:11:55 -0500

    Cambios en los repositorios.

---
+++ gest_proy_invest/src/main/java/com/example/gest_proy_invest/GestProyInvestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gest_proy_invest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestProyInvestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestProyInvestApplication.class, args);
+	}
+
+}
---
+++ gest_proy_invest/src/main/java/com/example/gest_proy_invest/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.gest_proy_invest.service;
+
+import com.example.gest_proy_invest.entity.User;
+
+public interface UserService {
+	public Iterable<User> getAllUsers();
+}
